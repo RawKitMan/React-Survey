c17728a9ac35bf43ca9185288fa0f1fa8e6dd5fd
Clifford Vaughn <cjvaughn@rocketmail.com>
2019-05-30 18:39:39 -0700

    Set up radio buttons and question for ....not survey...trivia...it's trivia now.

--- survey-project/src/pages/Survey/index.js
+++ survey-project/src/pages/Survey/index.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 
 
+let questions = {
+    1: { question: "Who is the son of King Varian Wrynn?", answers: ["Thrall", "Anduin", "Magni", "Cho'gall"] }
+};
+
+let questionNum = 1;
+// Work on making single choice radio buttons and how to submit the value.
 class Survey extends Component {
+
+    state = {
+        question: questions[questionNum].question,
+        guessedRight: 0,
+        guessedWrong: 0,
+        answers: questions[questionNum].answers
+    }
 
     render() {
         return (
-            <p>fart</p>
+            <Jumbotron>
+                <p>{this.state.question}</p>
+                {this.state.answers.map((answer) => {
+                    return (
+                        <Form>
+                            <div key={`inline-radio`} className="mb-3">
+                                <Form.Check inline label={answer} type="radio" id={`inline-radio-${answer}`} value = {answer} />
+                            </div>
+                        </Form>
+                    );
+                })}
+            </Jumbotron>
         );
     }
 };
