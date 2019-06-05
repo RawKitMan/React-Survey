75902f4471a1f77c8859c3f661cebbcf6b3526f8
Clifford Vaughn <cjvaughn@rocketmail.com>
2019-06-04 18:51:04 -0700

    Working on iterating through each question. I may have to make them their own pages instead and use a redux to keep track of scores.

--- survey-project/src/pages/Survey/index.js
+++ survey-project/src/pages/Survey/index.js
@@ -1,37 +1,61 @@
 import React, { Component } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 
 let questions = {
-    1: { question: "Who is the son of King Varian Wrynn?", answers: ["Thrall", "Anduin", "Magni", "Cho'gall"] }
+    1: { question: "Who is the son of King Varian Wrynn?", answers: ["Thrall", "Anduin", "Magni", "Cho'gall"] },
+    2: { question: "Who am I?", answers: ["Herp", "Derp", "CJ", "Nerp"] }
 };
 
-let questionNum = 1;
 // Work on making single choice radio buttons and how to submit the value.
 class Survey extends Component {
 
-    state = {
-        question: questions[questionNum].question,
-        guessedRight: 0,
-        guessedWrong: 0,
-        answers: questions[questionNum].answers
+    constructor(props) {
+        super(props);
+        this.state = {
+
+            guessedRight: 0,
+            guessedWrong: 0,
+            questionNum: 1
+
+        }
+
+        this.state.question = questions[this.state.questionNum].question
+       
+
+        this.state.answers = questions[this.state.questionNum].answers
+        
+    }
+
+
+    incrementQuestion = () => {
+
+        this.setState((state) => {
+            console.log(state);
+            return ({
+                questionNum: state.questionNum + 1
+            })
+        });
     }
 
     render() {
         return (
-            <Jumbotron>
+            <Jumbotron key={this.state.questionNum}>
                 <p>{this.state.question}</p>
                 {this.state.answers.map((answer) => {
                     return (
                         <Form>
                             <div key={`inline-radio`} className="mb-3">
-                                <Form.Check inline label={answer} type="radio" id={`inline-radio-${answer}`} value = {answer} />
+                                <Form.Check inline label={answer} type="radio" id={`inline-radio-${answer}`} value={answer} />
                             </div>
                         </Form>
+
                     );
                 })}
-            </Jumbotron>
+                <Button onClick={() => { this.incrementQuestion() }} type="submit" variant='danger' size="lg" active>Submit</Button>
+            </Jumbotron >
         );
     }
 };
