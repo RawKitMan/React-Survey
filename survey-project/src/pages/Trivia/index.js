ad5239dab1b373239f39cb25ec57fcc545f39031
CJ Vaughn <cjvaughn@rocketmail.com>
2019-06-16 14:45:14 -0700

    Set up a modal to show the user if the answer was correct or incorrect.

--- survey-project/src/pages/Trivia/index.js
+++ survey-project/src/pages/Trivia/index.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 //Get our score screen
 import ScoreScreen from "../../components/ScoreScreen";
 
-
+//The Trivia Component in all of its glory.
 class Trivia extends Component {
 
     //The state for our Trivia game
@@ -28,7 +29,18 @@ class Trivia extends Component {
         correctAnswers: [1, 2, 0],
         guessedAnswer: 0,
         questionNum: 0,
-        correct: false
+        correct: false,
+        show: false
+    }
+
+    //Closes the modal
+    handleClose() {
+        this.setState({ show: false });
+    }
+
+    //Opens the modal
+    handleShow() {
+        this.setState({ show: true });
     }
 
     //Method to move on to the next question
@@ -40,56 +52,96 @@ class Trivia extends Component {
 
     //Grabs the answer the user clicks on and stores it in the state for later comparison
     handleChange = (event) => {
-        
+
         this.setState({
             guessedAnswer: event.target.value
         })
     }
 
+    //We want to track how many the user gets right or gets wrong. We then change the correct state property based on the answer submitted.
     keepScore = () => {
-        console.log(typeof this.state.guessedAnswer);
-        console.log(this.state.correctAnswers[this.state.questionNum])
-        if (parseInt(this.state.guessedAnswer) === this.state.correctAnswers[this.state.questionNum]){
+        
+        if (parseInt(this.state.guessedAnswer) === this.state.correctAnswers[this.state.questionNum]) {
             this.setState((state) => ({
-                guessedRight: state.guessedRight + 1
+                guessedRight: state.guessedRight + 1,
+                correct: true
             }))
         }
-        else{
+        else {
             this.setState((state) => ({
-                guessedWrong: state.guessedWrong + 1
-            })) 
+                guessedWrong: state.guessedWrong + 1,
+                correct: false
+            }))
         }
     }
 
     //Put the questions and available answers out for the user to answer if there are 
     //questions left to ask. Otherwise, the page will display the final outcome.
     render() {
+
+        //Boolean value that helps us determine if we have any questions left to go.
         let haveMoreQuestions = this.state.questionNum < this.state.questions.length;
+
+
+        //The meat of it all. If we have questions left to answer, the page will show the question the user is currently on.
+        //The user clicks the radio button to submit their guess and submit it. A modal will appear telling the user if they 
+        //are correct or not.
+
+        //Once all of the questions have been asked, the user is taken to a score screen. If they guessed more than 50% correctly,
+        //the user wins.
         return (
             <div>
                 {haveMoreQuestions ? (
-                    <Jumbotron key={this.state.questionNum}>
-                        <p>{this.state.questions[this.state.questionNum]}</p>
-                        <Form>
-                            <Form.Group>
-                                {this.state.answers[this.state.questionNum].map((answer, index) => {
-                                    return (
-                                        <div>
-                                            <label>
-                                                <input
-                                                    type="radio"
-                                                    name="radio-answers"
-                                                    value={index}
-                                                    onChange={this.handleChange} />{answer}
-                                            </label>
-                                            <br />
-                                        </div>
-                                    );
-                                })}
-                            </Form.Group>
-                        </Form>
-                        <Button onClick={() => { this.incrementQuestion(); this.keepScore(); }} type="submit" variant='danger' size="lg" active>Submit</Button>
-                    </Jumbotron >
+                    <div>
+                        <Jumbotron key={this.state.questionNum}>
+                            <p>{this.state.questions[this.state.questionNum]}</p>
+                            <Form>
+                                <Form.Group>
+                                    {this.state.answers[this.state.questionNum].map((answer, index) => {
+                                        return (
+                                            <div>
+                                                <label>
+                                                    <input
+                                                        type="radio"
+                                                        name="radio-answers"
+                                                        value={index}
+                                                        onChange={this.handleChange} />{answer}
+                                                </label>
+                                                <br />
+                                            </div>
+                                        );
+                                    })}
+                                </Form.Group>
+                            </Form>
+                            <Button onClick={() => {
+                                this.handleShow();
+                                this.keepScore();
+                            }}
+                                type="submit"
+                                variant='danger'
+                                size="lg"
+                                active>
+                                Submit
+                        </Button>
+                        </Jumbotron >
+                        <Modal show={this.state.show} onHide={this.handleClose}>
+                            <Modal.Header closeButton>
+                            </Modal.Header>
+                            <Modal.Body>
+                                {(this.state.correct) ? (
+                                    <h2>Correct</h2>
+                                ) : (
+                                        <h2>Incorrect</h2>
+                                    )}
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="primary" onClick={() => {this.handleClose(); this.incrementQuestion();}}>
+                                    Next Question
+                                </Button>
+
+                            </Modal.Footer>
+                        </Modal>
+                    </div>
                 ) : (
                         <Jumbotron>
                             <ScoreScreen
@@ -103,6 +155,5 @@ class Trivia extends Component {
         );
     };
 };
-
 
 export default Trivia;
